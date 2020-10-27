81510633ba932994f391910dea1cdb6749234ec8
Val <vdecyatnik@gmail.com>
2020-10-26 17:19:46 -0700

    changed while loop to a for loop

--- Develop/script.js
+++ Develop/script.js
@@ -5,7 +5,7 @@ var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var specialChar = ['@', '#', '$', '%', '^', '&', '*', '!', '?'];
 var passwordLength = [];
 var userSelection = [];
-var charactersToUse= [];
+var userPassWord= [];
 
 
 
@@ -74,23 +74,43 @@ function generatePassword() {
 var password="";
 
 //WHILE password.length<passwordLength
-while( password.length< passwordLength){
+// while( password.length< passwordLength){
 
-  var charactersToUse=userSelection[Math.floor(Math.random())*userSelection.length];
+  for(var i=0;i<passwordLength;i++);
+
+   var userPassWord=userSelection[Math.floor(Math.random()*passwordLength)];
 
   
 
+  
+  
 
-}
+  
+
+ 
+
+
+
+console.log(passwordLength);
+console.log(userSelection);
+console.log(userPassWord);
+
+
 function writePassword(){
 
 
 var password= generatePassword();
+
+
+
+
+
+
+
+
 var passwordText= document.querySelector("#password");
 
 passwordText.value = password;
-
-
 
 
 
