5e36c3346d312223e39ab19c6ce9ae667da4ceae
Val <vdecyatnik@gmail.com>
2020-11-01 19:09:13 -0800

    readme

--- Develop/script.js
+++ Develop/script.js
@@ -1,57 +1,36 @@
-// Array List 
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialChar = ['@', '#', '$', '%', '^', '&', '*', '!', '?'];
+// Array List ]
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var specialChar = "@#$%^&*!?";
 var passwordLength = [];
 var userSelection = [];
-var userPassWord= [];
-
-
-
-
-
-
-let generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function generatePassword() {
 
-
-
-  var passwordLength = prompt("Choose between 8-128 characters for your password");
-  if (passwordLength < 8 || passwordLength > 128) {
+  var length = prompt("Choose between 8-128 characters for your password");
+  if (length < 8 || length > 128) {
     alert("please follow the instructions");
     return;
   }
-  console.log(passwordLength);
-
-
-  
-  
- //DECLARE a new list of "charactersToUse"
- charactersToUse=[];
+  console.log(length);
 
   var up = confirm("Would you like upper case letters in your password");
   if (up) //Then push upperCase into charactersToUse 
   {
-    userSelection.push(...upperCase)}
+    userSelection.push(...upperCase)
+  }
 
-
-  console.log(upperCase);
-
+  console.log(userSelection);
 
   var lo = confirm("Would you like lower case letters in your password?");
   if (lo)//Then push lowerCase into charactersToUse
-   {
-
+  {
     userSelection.push(...lowerCase)
   }
 
-
-
-
-  console.log(lowerCase);
+  console.log(userSelection);
 
 
   var nu = confirm("Would you like numbers in your password?");
@@ -59,77 +38,34 @@ function generatePassword() {
     userSelection.push(...numbers)
   }
 
-  console.log(numbers);
+  console.log(userSelection);
 
   var sp = confirm("Would you like special characters in your password?");
-  if (sp)
-{
-  userSelection.push(...specialChar)
-}
-  console.log(specialChar);
+  if (sp) {
+    userSelection.push(...specialChar)
+  }
+  console.log(userSelection);
+
+  for (var i = 0; i < length; i++) {
+    console.log(length[i]);
+
+    passwordLength.push(userSelection[Math.floor(Math.random() * userSelection.length)]);
+
+    console.log(passwordLength.join("+"));
+  }
+
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = passwordLength.join("");
 
 }
 
-//DECLARE new password strings
-var password="";
-
-//WHILE password.length<passwordLength
-// while( password.length< passwordLength){
-
-  for(var i=0;i<passwordLength;i++);
-
-   var userPassWord=userSelection[Math.floor(Math.random()*passwordLength)];
-
-  
-
-  
-  
-
-  
-
- 
-
-
-
-console.log(passwordLength);
-console.log(userSelection);
-console.log(userPassWord);
-
-
-function writePassword(){
-
-
-var password= generatePassword();
-
-
-
-
-
-
-
-
-var passwordText= document.querySelector("#password");
-
-passwordText.value = password;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
+let generateBtn = document.querySelector("#generate");
 // Add event listener to generate button
 generateBtn.addEventListener("click", generatePassword);
+
+
+
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # RandomPasswordGenerator
+
+
+I created a random password generator to help users create a secure password using different types of characters.
+
+## Javascript
+
+I used javascript functions and conditionals to create prompts to ask the user what type of characters they would like included in the password. I also confirmed how many characters the user wants. I used a join method to combine all the user selections or characters and length to create a new password string.
+
+## <img width="1280" alt="Screen Shot 2020-11-01 at 7 05 03 PM" src="https://user-images.githubusercontent.com/72056832/97826093-869b2d00-1c75-11eb-9e63-95adae665c00.png">
